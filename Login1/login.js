51ab55c4abf2bb4435f91b8c7e7ecdb48fc221b9
1NT18IS007 <82832823+1NT18IS007@users.noreply.github.com>
2021-07-18 18:24:47 +0530

    Add files via upload

---
+++ Login1/login.js
@@ -0,0 +1,11 @@
+var arr = Array();
+let count = 0;
+function myfunc()
+{
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("pd").value;
+    let gender = document.getElementById("gen").value;
+    let per = new Object(name.name.password.gender);
+
+    
+}
