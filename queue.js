3206d2724fa2382f7ec193e5a4b24f762630e2cc
1NT18IS007 <82832823+1NT18IS007@users.noreply.github.com>
2021-07-18 19:05:48 +0530

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,57 @@
+class Queue
+{
+    constructor()
+    {
+        this.items = [];
+    }
+    enqueue(ele)
+    {
+        this.items.push(ele);
+    }
+
+    dequeue()
+    {
+        if(this.isEmpty())
+            return "Underflow";
+        return this.items.shift();
+    }
+    front()
+    {
+        if(this.isEmpty())
+            return "Queue is empty";
+        return this.items[0];
+    }
+
+    isEmpty()
+    {
+        return this.items.length == 0;
+    }
+
+    printQueue()
+    {
+        var str = "";
+        for(var i=0;i<this.items.length;i++)
+        {
+            str += this.items[i] + " ";
+            
+        }
+        return str;
+    }
+}
+
+var queue = new Queue();
+
+console.log(queue.dequeue());
+console.log(queue.isEmpty());
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
+console.log(queue.front());
+console.log(queue.dequeue());
+console.log(queue.printQueue());
+console.log(queue.front());
+console.log(queue.dequeue());
+console.log(queue.printQueue());
